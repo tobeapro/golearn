42a7b7bb3aecef245c998934fdca713924ea18fe
liukai <471091127@qq.com>
2018-09-12 22:17:37 +0800

    websockt-demo

---
+++ websocket_demo/server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"github.com/gorilla/websocket"
+	"net/http"
+)
+
+var (
+	upgrader = websocket.Upgrader{
+		// 允许跨域
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+)
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	var (
+		conn *websocket.Conn
+		err  error
+		data []byte
+	)
+	if conn, err = upgrader.Upgrade(w, r, nil); err != nil {
+		return
+	}
+	for {
+		if _, data, err = conn.ReadMessage(); err != nil {
+			goto ERR
+		}
+		if err = conn.WriteMessage(websocket.TextMessage, data); err != nil {
+			goto ERR
+		}
+	}
+ERR:
+	conn.Close()
+}
+
+func main() {
+	http.HandleFunc("/ws", wsHandler)
+
+	http.ListenAndServe("0.0.0.0:77", nil)
+}
